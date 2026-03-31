27bb959bbe92767d8c8ea1b92e7bee93c9786e61
Piyumi Rameshka <piyumi.amarasinghebaragamage@populationgenomics.org.au>
2026-03-31 15:49:06 +1100

    Task/[SET-834] Setup pulumi script to install the IGV desktop python proxy (#1)
    This is the initial version of the IGV desktop Python proxy application.
    This PR contains,
    
    The Python script for proxy logic
    Pulumi scripts for infrastructure creation
    GitHub workflows

---
+++ infrastructure/__main__.py
@@ -0,0 +1,151 @@
+import pulumi
+import pulumi_docker as docker
+import pulumi_gcp as gcp
+from pulumi import Config, get_stack
+from pulumi_docker import BuilderVersion
+
+stack = get_stack()
+gcp_config = Config('gcp')
+app_config = Config('app')
+
+# set up artifact registry
+gcp.artifactregistry.Repository(
+    'igv-desktop-proxy-repository',
+    location=gcp_config.require('region'),
+    repository_id=f'igv-desktop-proxy-repository-{stack}',
+    format='DOCKER',
+    description='igv-desktop-proxy docker repository',
+    # Remove any versions that are older than 30 days and are untagged
+    # this means that the latest version will always be kept but older
+    # versions that are already deployed will not.
+    cleanup_policies=[
+        {
+            'id': 'delete-untagged',
+            'action': 'DELETE',
+            'condition': {'tag_state': 'UNTAGGED', 'older_than': '30d'},
+        },
+    ],
+)
+
+
+image = docker.Image(
+    'igv-desktop-proxy-image',
+    image_name=f'{gcp_config.require("region")}-docker.pkg.dev/{gcp_config.require("project")}/igv-desktop-proxy-repository-{stack}/igv-desktop-proxy-image:latest',
+    build=docker.DockerBuildArgs(
+        context='../',
+        dockerfile='Dockerfile',
+        args={
+            'BUILDKIT_INLINE_CACHE': '1',
+        },
+        builder_version=BuilderVersion.BUILDER_BUILD_KIT,
+        platform='linux/amd64',
+    ),
+)
+
+
+service_account = gcp.serviceaccount.Account(
+    'igv-desktop-proxy-service-account',
+    account_id=f'igv-desktop-proxy-{stack}',
+    display_name=f'IGV Desktop Proxy ({stack})',
+)
+
+cloud_run = gcp.cloudrunv2.Service(
+    'igv-desktop-proxy',
+    name=f'igv-desktop-proxy-{stack}',
+    ingress='INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER',
+    location=gcp_config.require('region'),
+    default_uri_disabled=True,
+    template=gcp.cloudrunv2.ServiceTemplateArgs(
+        service_account=service_account.email,
+        timeout='3600s',  # Max cloudrun timeout is 1 hour
+        scaling=gcp.cloudrunv2.ServiceTemplateScalingArgs(
+            min_instance_count=0,
+            max_instance_count=10,
+        ),
+        containers=[
+            gcp.cloudrunv2.ServiceTemplateContainerArgs(
+                image=image.repo_digest,
+                resources=gcp.cloudrunv2.ServiceTemplateContainerResourcesArgs(
+                    limits={
+                        'memory': '4Gi',
+                        'cpu': '2',
+                    },
+                    startup_cpu_boost=True,  # Allocate extra CPU during startup to improve cold start times
+                ),
+                envs=[],
+                ports=gcp.cloudrunv2.ServiceTemplateContainerPortsArgs(
+                    name='http1',
+                    container_port=8080,
+                ),
+            ),
+        ],
+    ),
+)
+
+# Allow cloud run unauthenticated access
+gcp.cloudrunv2.ServiceIamMember(
+    'igv-desktop-proxy-public-access-binding',
+    project=cloud_run.project,
+    location=cloud_run.location,
+    name=cloud_run.name,
+    role='roles/run.invoker',
+    member='allUsers',
+)
+
+
+neg = gcp.compute.RegionNetworkEndpointGroup(
+    'igv-desktop-proxy-neg',
+    network_endpoint_type='SERVERLESS',
+    region=gcp_config.require('region'),
+    cloud_run=gcp.compute.RegionNetworkEndpointGroupCloudRunArgs(
+        service=cloud_run.name,
+    ),
+)
+
+
+backend_service = gcp.compute.BackendService(
+    'igv-desktop-proxy-backend-service',
+    enable_cdn=False,
+    log_config=gcp.compute.BackendServiceLogConfigArgs(enable=True),
+    protocol='HTTPS',
+    backends=[gcp.compute.BackendServiceBackendArgs(group=neg.id)],
+)
+
+
+ip_address = gcp.compute.GlobalAddress(
+    'igv-desktop-proxy-ip-address',
+    name=f'igv-desktop-proxy-ip-address-{stack}',
+)
+
+ssl_cert = gcp.compute.ManagedSslCertificate(
+    'igv-desktop-proxy-ssl-cert',
+    name=f'igv-desktop-proxy-ssl-cert-{stack}',
+    managed={
+        'domains': [app_config.require('domain')],
+    },
+)
+
+url_map = gcp.compute.URLMap(
+    'igv-desktop-proxy-url-map',
+    name=f'igv-desktop-proxy-url-map-{stack}',
+    default_service=backend_service.id,
+)
+
+https_proxy = gcp.compute.TargetHttpsProxy(
+    'igv-desktop-proxy-https-proxy',
+    name=f'igv-desktop-proxy-https-proxy-{stack}',
+    url_map=url_map.id,
+    ssl_certificates=[ssl_cert.id],
+)
+
+# Setup Global forwarding rule
+gcp.compute.GlobalForwardingRule(
+    'igv-desktop-proxy-forwarding-rule',
+    name=f'igv-desktop-proxy-forwarding-rule-{stack}',
+    target=https_proxy.id,
+    port_range='443',
+    ip_address=ip_address.address,
+)
+
+
+pulumi.export('load balancer ip', ip_address.address)
---
+++ server/main.py
@@ -0,0 +1,111 @@
+import logging
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
+import httpx
+import uvicorn
+from fastapi import Depends, FastAPI, Request, Response
+from fastapi.responses import StreamingResponse
+
+# Application constants
+DEFAULT_PORT = 8080
+GCS_BASE_URL = 'storage-download.googleapis.com'
+REQUEST_URL_PARTS = 2
+CLIENT_TIMEOUT = 5  # httpx default timeout 5 secs
+
+logging.getLogger().setLevel(logging.INFO)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    """Define application lifespan.
+
+    https://fastapi.tiangolo.com/advanced/events/#startup-and-shutdown-together.
+    """
+    app.state.httpx_client = httpx.AsyncClient(timeout=CLIENT_TIMEOUT)
+    yield
+    await app.state.httpx_client.aclose()
+
+
+def get_httpx_client(request: Request) -> httpx.AsyncClient:
+    """Retrieve the shared httpx.AsyncClient instance."""
+    return request.app.state.httpx_client
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.api_route('/health', methods=['GET'])
+async def health_check(_request: Request):
+    """Return health check response."""
+    return Response(status_code=200, content='OK')
+
+
+@app.api_route('/{full_path:path}', methods=['GET'])
+async def proxy_handler(request: Request, full_path: str, client: httpx.AsyncClient = Depends(get_httpx_client)):
+    """Proxy requests to GCS."""
+    path_segments = full_path.split('/', 1)
+    if len(path_segments) < REQUEST_URL_PARTS:
+        return Response(content='Invalid path format. Use /bucket/path', status_code=400)
+
+    bucket = path_segments[0]
+    object_path = path_segments[1]
+    target_url = httpx.URL(scheme='https', host=f'{bucket}.{GCS_BASE_URL}', path=f'/{object_path}')
+
+    headers = {}
+    if 'authorization' in request.headers:
+        headers['Authorization'] = request.headers['authorization']
+    if 'range' in request.headers:
+        headers['Range'] = request.headers['range']
+
+    try:
+        req = client.build_request(
+            method=request.method,
+            url=target_url,
+            headers=headers,
+            params=request.query_params,
+        )
+
+        gcs_response = await client.send(req, stream=True)
+        gcs_response.raise_for_status()
+
+        # https://fastapi.tiangolo.com/advanced/custom-response/#streamingresponse
+        async def stream_wrapper() -> AsyncIterator[bytes]:
+            try:
+                async for chunk in gcs_response.aiter_raw():
+                    yield chunk
+            finally:
+                await gcs_response.aclose()
+
+        return StreamingResponse(
+            stream_wrapper(),
+            status_code=gcs_response.status_code,
+            headers=dict(gcs_response.headers),
+        )
+
+    except httpx.RequestError as exc:
+        logging.error(f'An error occurred while requesting {exc.request.url!r}. {exc}')
+        return Response(status_code=500, content='Internal Server Error')
+    except httpx.HTTPStatusError as exc:
+        logging.error(f'An error occurred while requesting {exc.request.url!r}. {exc}')
+        await exc.response.aread()
+        return Response(status_code=exc.response.status_code, content=exc.response.text)
+    except Exception as e:  # noqa: BLE001
+        logging.error(f'Unexpected error: {e}')
+        return Response(status_code=500, content='Internal Server Error')
+
+
+if __name__ == '__main__':
+    logging.info('##### Started reverse proxying server #####')
+
+    uvicorn.run(
+        'server.main:app',
+        host='localhost',
+        port=DEFAULT_PORT,
+    )
