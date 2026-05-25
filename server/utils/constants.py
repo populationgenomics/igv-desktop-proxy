2c3feaaeab75db24ef09a8e303e9ae8078256492
piyumiamarasinghe <rameshkafox@gmail.com>
2026-05-25 11:35:10 +1000

    [SET-1081] address review comments

--- server/utils/constants.py
+++ server/utils/constants.py
@@ -10,14 +10,11 @@ HTTPX_CLIENT_TIMEOUT = 5  # httpx default timeout 5 secs
 # rate limiting caps
 DOWNLOAD_CAP_BYTES = 1073741824  # bytes (1GB download cap enforced)
 CAPPED_TIME_WINDOW_SECS = 3600  # 1-hour
-FIRST_BYTE_RANGE = 'bytes=0-511999'
 
 # rate limit retry
 LOCK_POLL_ATTEMPTS = 10
 LOCK_POLL_SLEEP_S = 0.1
 
-CRAM_INDEX_FILE_EXTENSION = '.crai'
-BAM_INDEX_FILE_EXTENSION = '.bai'
 SUB_PREFIX = 'sub'
 LOCK_PREFIX = 'lock'
 TOKEN_HASH_PREFIX = 'token_hash'  # noqa: S105
